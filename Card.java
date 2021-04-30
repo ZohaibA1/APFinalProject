950739eef2d68202e8a32033b0ee08a3a8ae7235
markverrajr1701 <ma09verr@siena.edu>
2021-04-30 19:55:29 -0400

    Started implementation on Card and Suit classes.

---
+++ Card.java
@@ -0,0 +1,17 @@
+
+/**
+ * A class designed to implement functionality as part of a fully functional poker game, and possibly a Blackjack game.
+ * This class will represent Card objects in a poker or blackjack game, as they are represented in the real world.
+ *
+ * @author Mark Verra
+ * @version Spring 2021
+ */
+public class Card
+{
+    // instance variable to denote a given card's suit (e.g., Hearts, Diamonds, etc.)
+    
+    // instance variable to denote a given card's rank (e.g., 2's, 10's, Kings, etc.)
+    
+    // instance variable to denote a given card's value (e.g, 1, 2, 3, 11, etc)
+    
+}
---
+++ Suit.java
@@ -0,0 +1,11 @@
+
+/**
+ * A class designed to implement functionality as part of a fully functional poker game, 
+ *
+ * @author Mark Verra
+ * @version Spring 2021
+ */
+public class Suit
+{
+    
+}
